45f63932d6c58d0d440d65d88acbeb0f0901b8d8
panix <panix>
2026-07-08 05:30:53 +0530

    coep update to credentialless

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env fish
+#!/usr/bin/sh
 
-npx github:http-party/http-server zig-out/web -p 8000  --header "Cross-Origin-Opener-Policy: same-origin" --header "Cross-Origin-Embedder-Policy: require-corp"
+npx github:http-party/http-server zig-out/web -p 8000  --header "Cross-Origin-Opener-Policy: same-origin" --header "Cross-Origin-Embedder-Policy: credentialless"
