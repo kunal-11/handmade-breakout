0b9655b984b71104f7ec6969577fb1f099b5b1e8
kunal-11 <kunalmchoudhary@gmail.com>
2026-09-23 00:28:16 +0530

    Update dev.sh
    npx allow git fetches

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-npx github:http-party/http-server zig-out/web -p 8000  --header "Cross-Origin-Opener-Policy: same-origin" --header "Cross-Origin-Embedder-Policy: credentialless"
+NPM_CONFIG_ALLOW_GIT=all npx github:http-party/http-server zig-out/web -p 8000  --header "Cross-Origin-Opener-Policy: same-origin" --header "Cross-Origin-Embedder-Policy: credentialless"
